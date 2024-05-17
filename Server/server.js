c3c7b5406e111ad56b8e90ccc6e111f82635babb
Hector Ramirez <67031386+hectorlil48@users.noreply.github.com>
2024-05-16 21:00:54 -0500

    connected to server

--- Server/server.js
+++ Server/server.js
@@ -1,37 +1,33 @@
-import express from 'express'
-import cors from 'cors';
-import dotenv from 'dotenv';
-import sequelize from './config/db.js'; 
-import router from './routes/users.route.js'
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import sequelize from "./config/db.js";
+import router from "./routes/users.route.js";
 /*Using sequelize to allow for better interactivity with the database by using 
 javascript objects and methods instead of raw queries. Maintains better readability*/
 
 dotenv.config();
 
-
 const app = express();
 app.use(express.json(), cors());
 
 //Testing the connection to mysql database here:
-sequelize.authenticate()
+sequelize
+  .authenticate()
   .then(() => {
-    console.log('Connection has been established successfully.');
+    console.log("Connection has been established successfully.");
   })
-  .catch(err => {
-    console.error('Unable to connect to the database:', err);
+  .catch((err) => {
+    console.error("Unable to connect to the database:", err);
   });
 
-
-sequelize.sync()
-  .then(() => {
-    console.log('Database & tables created!');
-  });
+sequelize.sync().then(() => {
+  console.log("Database & tables created!");
+});
 
 //Middleware:
-app.use('/api', router)
-
-
+app.use("/api", router);
 
 app.listen(process.env.PORT, () => {
-    console.log("Server is running on port" + process.env.PORT);
+  console.log("Server is running on port" + process.env.PORT);
 });
