f4ad0ac3d359b218925078e2eb83aca5e0536e1d
Luis Chavez <luis12chavez@gmail.com>
2024-05-20 13:35:51 -0700

    added some logic to user controller and logout feature to Dashboard

--- client/src/views/Dashboard.jsx
+++ client/src/views/Dashboard.jsx
@@ -1,7 +1,9 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 const Dashboard = () => {
+    const navigate = useNavigate()
     const [users, setUsers] = useState([]);
     const [error, setError] = useState(null);
 
@@ -27,9 +29,21 @@ const Dashboard = () => {
                 setError(err.message);
             });
     }, []);
+
+    const logoutHandler = () => {
+        axios.post('http://localhost:8000/api/logout', {}, {withCredentials: true})
+            .then((res) => {
+                navigate('/')
+            })
+            .catch((error) => {
+                console.log(error)
+            })
+    }
+
     return (
         <div className="container">
             <h1>Dashboard</h1>
+            <button onClick={logoutHandler}>Logout</button>
             <table className="table table-secondary table-bordered">
                 <thead>
                     <tr>
