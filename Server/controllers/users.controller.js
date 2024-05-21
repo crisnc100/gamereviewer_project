314645509d1d980db17ad890f3b96c32cebbdaad
Luis Chavez <luis12chavez@gmail.com>
2024-05-20 18:52:57 -0700

    .

--- Server/controllers/users.controller.js
+++ Server/controllers/users.controller.js
@@ -68,7 +68,7 @@ const UserController = {
 
       res.status(200).json({ message: "Logged in successfully", user, token });
     } catch (err) {
-      // res.status(500).json({ error: err.message });
+      res.status(500).json({ error: err.message });
     }
   },
 
