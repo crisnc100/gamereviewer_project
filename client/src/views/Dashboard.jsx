575ce167efb54a4a18d86bde6fd4f10f9656f851
Hector Ramirez <67031386+hectorlil48@users.noreply.github.com>
2024-05-21 22:56:46 -0500

    Added styling to dashboard

--- client/src/views/Dashboard.jsx
+++ client/src/views/Dashboard.jsx
@@ -32,26 +32,20 @@ const Dashboard = (props) => {
   return (
     <div className="container">
       <h1>Welcome {currentUser.firstName}!</h1>
-      <h3>Game Reviews</h3>
-
-      <table className="table table-secondary table-bordered">
-        <thead>
-          <tr>
-            <th>First Name</th>
-            <th>Last Name</th>
-            <th>Email</th>
-          </tr>
-        </thead>
-        <tbody>
-          {reviews.map((review, index) => (
-            <tr key={index}>
-              <td>{review.User.firstName}</td>
-              <td>{review.Game.title}</td>
-              <td>{review.rating}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+      <h3 className="text-center">User Reviews</h3>
+      <div className=" d-flex flex-column align-items-center">
+        {reviews.map((review, index) => (
+          <div key={index} className="border-bottom border-secondary w-75 p-3">
+            <h4 className="fw-bold">
+              {review.User.firstName} {review.User.lastName} on{" "}
+              {review.Game.title}
+            </h4>
+            <p>{review.reviewText}</p>
+            <p>{new Date(review.created_at).toLocaleDateString()}</p>
+            <p>{review.rating}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
